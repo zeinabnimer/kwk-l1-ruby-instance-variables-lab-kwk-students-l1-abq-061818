e575180ba8980a33d8f8088484040123cbcd6e7b
zeinabnimer <www.zeinab6@gmail.com>
2018-06-22 19:50:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+class dog
+  def name= (dog_name)
+    this_dogs_name = dog_name
+  end
+
+  def name
+    this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "lassie"
+
+lassie.name
